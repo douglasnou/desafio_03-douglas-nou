fe731ce796985574eef83ab625375ce7bc191dff
Douglas Nou <dodonou3@gmail.com>
2024-05-24 18:51:39 -0300

    first commit

---
+++ scripts/habilidades.js
@@ -0,0 +1,7 @@
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
---
+++ scripts/projetos.js
@@ -0,0 +1,7 @@
+$('.carousel-2').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
